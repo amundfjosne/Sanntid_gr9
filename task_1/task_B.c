fe10225640a24066cf9910fc290b2f0a739093f6
nadiawangberg <nadiagw@stud.ntnu.no>
2019-09-10 10:24:59 +0200

    add task B, rdtsc done

---
+++ task_1/task_B.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <time.h>
+#include <unistd.h>
+#include <x86intrin.h>
+
+unsigned long long rdtsc(){
+		unsigned int lo,hi;
+		__asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+		return ((unsigned long long)hi << 32) | lo;
+}
+
+void task_b_rdtsc() {
+	unsigned long long initial, current;
+	initial = rdtsc();
+	for(int i = 0; i < 10*1000*1000; i++){
+    rdtsc();
+    }
+	current = rdtsc();
+
+	printf("Time average = %f\n", (current-initial)/(10*1000*1000)/(2.6));
+
+}
+
+void task_b_clock_gettime() {
+	unsigned long long initial, current;
+	initial = rdtsc();
+	for(int i = 0; i < 10*1000*1000; i++){
+    rdtsc();
+    }
+	current = rdtsc();
+
+	printf("Time average = %f\n", (current-initial)/(10*1000*1000)/(2.6));
+
+}
+
+
+
+void main(void) {
+	task_b_rdtsc();
+}
